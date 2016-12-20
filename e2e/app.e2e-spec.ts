6ce0c80209f13d157edb75c3f3a160701e7f2c0c
Nuntipat Narkthong <nuntipat@gmail.com>
2016-12-21 00:08:09 +0700

    Create new project using angular-cli and install bootstrap and ng2-bootstrap

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { MpWebpackPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('mp-webpack App', function() {
+  let page: MpWebpackPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new MpWebpackPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
