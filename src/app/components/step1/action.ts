39fd453940c8bd1b8a68834740754bc446adafd4
Unknown <Bright130@hotmail.com>
2017-01-08 02:06:50 +0700

    Fixed bug

--- src/app/components/step1/action.ts
+++ src/app/components/step1/action.ts
@@ -7,17 +7,17 @@ export interface ActionGroup {
 export interface Action {
   id: number;
   name: string;
-  type: ActionType[];
-  description: string;
+  type: string[];
+  description: ActionType[];
   property?: ActionProperty[];
   image: string;  // path to an image that will be used to represent this action in the canvas
 }
+export enum ActionType{
+ Accel_X,
+ Accel_Y,
+ Unspecified //to be removed
+ // TODO: add new actions
 
-export enum ActionType {
-  Accel_X,
-  Accel_Y,
-  Unspecified // to be removed
-  // TODO: add new actions
 }
 
 export interface ActionProperty {
