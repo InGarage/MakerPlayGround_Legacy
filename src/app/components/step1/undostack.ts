b91b867f0934d3000941723319c0035ccf6bb699
Nuntipat Narkthong <nuntipat@gmail.com>
2017-01-07 13:00:42 +0700

    Add UndoStack class without merging it into middle component

--- src/app/components/step1/undostack.ts
+++ src/app/components/step1/undostack.ts
@@ -1,3 +1,32 @@
-export class UndoStack {
-    
+
+export class UndoStack<T> {
+    private data: T[] = [];
+    private i: number = 0;
+
+    push(item: T) {
+        if (this.canRedo()) {
+            this.data.splice(this.i, this.data.length - this.i, item);
+        } else {
+            this.data.push(item);
+        }
+        this.i++;
+    }
+
+    undo() : T {
+        this.i--;
+        return this.data[this.i - 1];
+    }
+
+    canRedo() : boolean {
+        return !(this.i === this.data.length);
+    }
+
+    redo() : T {
+        // return undefined if we can't redo
+        if (this.i === this.data.length)
+            return undefined;
+
+        this.i++;
+        return this.data[this.i - 1];
+    }
 }
