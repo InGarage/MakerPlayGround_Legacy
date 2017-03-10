5fad782f916e17260cd80c833e77e3a3c45b57c9
Nuntipat Narkthong <nuntipat@gmail.com>
2017-03-10 11:08:33 +0700

    Add route gauge and not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, RequestOptions, JsonpModule } from '@angular/http';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
 import { TreeModule } from 'angular2-tree-component';
-import {MdGridListModule} from '@angular/material';
+import { MdGridListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+
 import { LoginComponent } from './login.component';
 import { HomeComponent } from './components/home.component';
 import { TutorialComponent } from './components/tutorial.component';
@@ -16,31 +18,34 @@ import { Step3Component } from './components/step1/step3.component';
 import { ActionComponent } from './components/step1/action.component';
 import { MiddleComponent } from './components/step1/middle.component';
 import { PropertyComponent } from './components/step1/property.component';
+import { PageNotFoundComponent } from './not-found.component';
 
 import { Auth } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 import { ProjectService } from './services/projects.service';
-import { routing } from './app.routing';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
-  return new AuthHttp(new AuthConfig({
-    tokenName: 'id_token',
-  }), http, options);
+    return new AuthHttp(new AuthConfig({
+        tokenName: 'id_token',
+    }), http, options);
 }
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpModule, JsonpModule, routing, TreeModule, MdGridListModule],
-  declarations: [AppComponent, LoginComponent, HomeComponent, TutorialComponent,
-    SampleProjectComponent, Step1Component, ActionComponent,
-    MiddleComponent, PropertyComponent, Step3Component],
-  providers: [
-    {
-      provide: AuthHttp,
-      useFactory: authHttpServiceFactory,
-      deps: [Http, RequestOptions]
-    },
-    ProjectService,
-    Auth
-  ],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule, HttpModule, JsonpModule, TreeModule, MdGridListModule, AppRoutingModule],
+    declarations: [AppComponent, LoginComponent, HomeComponent, TutorialComponent,
+        SampleProjectComponent, Step1Component, ActionComponent,
+        MiddleComponent, PropertyComponent, Step3Component, PageNotFoundComponent],
+    providers: [
+        {
+            provide: AuthHttp,
+            useFactory: authHttpServiceFactory,
+            deps: [Http, RequestOptions]
+        },
+        ProjectService,
+        Auth,
+        AuthGuard
+    ],
+    bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
